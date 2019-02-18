580337b6c7984df98b8bf39f2db9e40fbb1c2c9c
Patrick Hussey <patrick.hussey@me.com>
2019-02-18 11:01:41 +0000

    Fixed install metasploit

--- LinuxManagementTools/UbuntuSetUp.sh
+++ LinuxManagementTools/UbuntuSetUp.sh
@@ -18,43 +18,12 @@ rooted=$(whoami)
 
 instMSF(){
 
-add-apt-repository pps:webupd8team/java
-apt update
-apt install -y build-essential git libreadline-dev libssl-dev libpq5 libpq5-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxm12-dev libxslt1-dev libyaml-dev curl zlib1g-dev gawk bison libffi-dev libgdbm-dev libncurses5-dev libtool sqlite3 libgmp-dev gnupg2 dirmgr nmap
-apt update
-apt upgrade -y
-sleep 0.05
-cd ~
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
-sleep 0.05
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-#sudo plugin so we can run metasploit as root with "rbenv sudo msfconsole"
-git clone git://github.com/dcarley/rbenv-sudo.git ~/.rbenv/plugins/rbenv-sudo
-exec $SHELL
-RUBYVERSION=$(wget https://raw.githubusercontent.com/rapid7/metasploit-framework/master/.ruby-version -q -O - )
-rbenv install $RUBYVERSION
-rbenv global $RUBYVERSION
-ruby -v
-sleep 0.05
-cd /opt
-git clone https://github.com/rapid7/metasploit-framework.git
-chown -R `whoami` /opt/metasploit-framework
-cd metasploit-framework
-gem install bundler
-bundle install
-sleep 0.05
-cd metasploit-framework
-bash -c 'for MSF in $(ls msf*); do ln -s /opt/metasploit-framework/$MSF /usr/local/bin/$MSF;done'
-sleep 0.05
-echo "export PATH=$PATH:/usr/lib/postgresql/10/bin" >> ~/.bashrc
-. ~/.bashrc
-usermod -a -G postgres `whoami`
-su - `whoami`
-cd /opt/metasploit-framework
-./msfdb init
+apt install -y git curl
+
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+  chmod 755 msfinstall && \
+  ./msfinstall
+
 }
 
 grubCustom(){
